39488d439d66f1320b47dd0b51d64713550346ed
Tom <tomaszsobkowicz21@gmail.com>
2020-08-31 21:57:28 +0200

    Some changes in Scream component, create login page

--- src/components/Scream.js
+++ src/components/Scream.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
 import PropTypes from 'prop-types';
+import dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
 
 // MUI components
 import Card from '@material-ui/core/Card';
@@ -34,6 +36,8 @@ const Scream = ({ scream }) => {
     // commentCount,
   } = scream;
 
+  dayjs.extend(relativeTime);
+
   const classes = useStyles();
 
   return (
@@ -53,7 +57,7 @@ const Scream = ({ scream }) => {
           {userHandle}
         </Typography>
         <Typography variant="body2" color="textSecondary">
-          {createdAt}
+          {dayjs(createdAt).fromNow()}
         </Typography>
         <Typography variant="body1">{body}</Typography>
       </CardContent>
--- src/pages/login.js
+++ src/pages/login.js
@@ -1,10 +1,137 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import axios from 'axios';
+import { useHistory, Link } from 'react-router-dom';
+
+// MUI components
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import CircularProgress from '@material-ui/core/CircularProgress';
+
+// Icons and other stuff
+import AppIcon from '../images/icon.png';
+
+const useStyles = makeStyles({
+  form: {
+    textAlign: 'center',
+  },
+  image: {
+    margin: '20px auto 20px auto',
+  },
+  pageTitle: {
+    margin: '10px auto 10px auto',
+  },
+  textField: {
+    margin: '10px auto 10px auto',
+  },
+  button: {
+    marginTop: 20,
+    position: 'relative',
+  },
+  customError: {
+    color: 'red',
+    fontSize: '0.8rem',
+    marginTop: 10,
+  },
+  progress: {
+    position: 'absolute',
+  },
+});
 
 const Login = () => {
+  const history = useHistory();
+  const [form, setForm] = useState({
+    email: '',
+    password: '',
+  });
+  const [loading, setLoading] = useState(false);
+  const [errors, setErrors] = useState({});
+  const { email, password } = form;
+  const classes = useStyles();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    const userData = {
+      email,
+      password,
+    };
+    try {
+      const { data } = await axios.post('/login', userData);
+      console.log(data);
+      setLoading(false);
+      history.push('/');
+    } catch (err) {
+      setErrors(err.response.data);
+      setLoading(false);
+    }
+  };
+
+  const handleChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+
   return (
-    <div>
-      <h1>login page</h1>
-    </div>
+    <Grid container className={classes.form}>
+      <Grid item sm />
+      <Grid item sm>
+        <img src={AppIcon} alt="monkey" className={classes.image} />
+        <Typography variant="h2" className={classes.pageTitle}>
+          Login
+        </Typography>
+        {/* Add noValidate becouse html in default wants to valideate email field */}
+        <form noValidate onSubmit={handleSubmit}>
+          <TextField
+            id="email"
+            name="email"
+            type="email"
+            label="email"
+            value={email}
+            helperText={errors.email}
+            error={!!errors.email}
+            onChange={handleChange}
+            fullWidth
+            className={classes.textField}
+          />
+          <TextField
+            id="password"
+            name="password"
+            type="password"
+            label="password"
+            value={password}
+            helperText={errors.password}
+            error={!!errors.password}
+            onChange={handleChange}
+            fullWidth
+            className={classes.textField}
+          />
+          {errors.general && (
+            <Typography variant="body2" className={classes.customError}>
+              {errors.general}
+            </Typography>
+          )}
+          <Button
+            type="submit"
+            variant="contained"
+            color="primary"
+            className={classes.button}
+            disabled={loading}
+          >
+            Login
+            {loading && (
+              <CircularProgress size={30} className={classes.progress} />
+            )}
+          </Button>
+          <br />
+          <small>
+            dont have an account? sign up <Link to="/signup">here</Link>
+          </small>
+        </form>
+      </Grid>
+      <Grid item sm />
+    </Grid>
   );
 };
 
